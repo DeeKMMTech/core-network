6e08b1e5458cb93ccfd91e2bbd0c6048e045439c
Dee <hein.h@siriustech.io>
2024-04-22 13:26:08 +0700

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
-import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
-
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
-    kotlin("plugin.serialization") version "1.9.22"
+    alias(libs.plugins.kotlinSerialization)
 }
 
 
@@ -35,7 +32,7 @@ kotlin {
             api(libs.ktor.serialization.kotlinx.json)
             api(libs.ktor.logging)
             api(libs.ktor.auth)
-            api(project(":shared:core-common"))
+            implementation(projects.shared.coreCommon)
         }
 
         androidMain.dependencies {
